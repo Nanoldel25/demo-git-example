48b13122c6e4045ebc6e639d8fba20116671ca94
Nanoldel <nanoldelitnelza@yandex.ru>
2025-04-01 12:35:23 +0300

    Func demo map complete

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 def demo_map():
-    # TODO...
-    # WIP = work in progress
-    pass
+   list1 = [1, 2, 3, 4]
+   list2 = [3, 4, 5, 6]
 
+   list_of_powers = list(map(pow, list1, list2))
+   print(list_of_powers)
 
 def main():
-    print('Hello main')
-
+    # print('Hello main')
+    demo_map()
 
 if __name__ == "__main__":
     main()
